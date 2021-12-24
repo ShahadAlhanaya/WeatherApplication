f1e0802ec3cffc78b1ce86eda9377236cd2323de
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-12-24 15:33:58 +0300

    icons added

--- WeatherApplication/Controller/WeatherViewController.swift
+++ WeatherApplication/Controller/WeatherViewController.swift
@@ -3,11 +3,13 @@
 //  WeatherApplication
 //
 //  Created by Shahad Nasser on 23/12/2021.
-//
 
 import UIKit
 
 class WeatherViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate{
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var modeButton: UIButton!
+    
     @IBOutlet weak var hourlyCollectionView: UICollectionView!
     @IBOutlet weak var dailyCollectionView: UICollectionView!
     
@@ -22,13 +24,16 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
     @IBOutlet weak var feelsLikeLabel: UILabel!
     
     
-    
     var current: Current?
     var hourlyList: [Current]?
     var dailyList: [Daily]?
+    var timeOffset = 0
+
 
     private var pendingWorkItem: DispatchWorkItem?
     let queue = DispatchQueue(label: "GetWeather")
+    
+    var isDark = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,13 +47,13 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
     func fetch(){
         pendingWorkItem?.cancel()
         let newWorkItem = DispatchWorkItem {
-             self.getTasks()
+             self.getWeather()
         }
         pendingWorkItem = newWorkItem
         queue.sync(execute: newWorkItem)
     }
     
-    func getTasks(){
+    func getWeather(){
         WeatherModel.getWeather(completionHandler: {data,response,error in
             guard let weatherData = data else { return }
             do{
@@ -56,25 +61,23 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
                 let jsonResult = try decoder.decode(WeatherResponse.self, from: weatherData)
 
                 self.current = jsonResult.current
-                self.hourlyList = jsonResult.hourly
+                self.hourlyList = jsonResult.hourly.slice(size: 24)[0]
                 self.dailyList = jsonResult.daily
-//                self.items = jsonResult
-//                print(jsonResult)
+                self.timeOffset = jsonResult.timezoneOffset
+
                 DispatchQueue.main.async {
-                    self.currentDateLabel.text = self.date(timestamp: self.current?.dt, format: "d MMM yyyy  h:mm a")
+                    self.currentDateLabel.text = self.date(timestamp: self.current?.dt ?? 0, format: "d MMM yyyy  h:mm a")
                     self.currentDescriptionLabel.text =  self.current?.weather[0].weatherDescription.rawValue
                     self.currentWeatherLabel.text = self.convertTempreture(temp: self.current?.temp ?? 0, from: .kelvin, to: .celsius)
-                    
                     
                     self.windLabel.text = "\(self.current?.windSpeed ?? 0)"
                     self.humidityLabel.text = "\(self.current?.humidity ?? 0)"
                     self.pressureLabel.text = "\(self.current?.pressure ?? 0)"
                     self.feelsLikeLabel.text = self.convertTempreture(temp: self.current?.feelsLike ?? 0, from: .kelvin, to: .celsius)
                     
-                    self.showUIElements(true)
                     self.hourlyCollectionView.reloadData()
                     self.dailyCollectionView.reloadData()
-//                    self.tableView.reloadData()
+                    self.showUIElements(true)
                 }
             }catch{
                 print(error)
@@ -82,12 +85,12 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
         })
     }
     
-    func date(timestamp: Int?, format: String)-> String{
-        let date = Date(timeIntervalSince1970: Double(timestamp ?? 0))
+    func date(timestamp: Int, format: String)-> String{
+        let date = Date(timeIntervalSince1970: Double(timestamp + timeOffset))
         let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone(abbreviation: "GMT") //Set timezone that you want
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
         dateFormatter.locale = NSLocale.current
-        dateFormatter.dateFormat = format //Specify your format that you want
+        dateFormatter.dateFormat = format
         return dateFormatter.string(from: date)
     }
     
@@ -98,6 +101,30 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
             let input = Measurement(value: temp, unit: inputTempType)
             let output = input.converted(to: outputTempType)
             return mf.string(from: output)
+    }
+    
+    func getIcon(icon: String)-> String{
+        switch icon{
+        case "01d", "01n":
+            return "sun.max"
+        case "02d", "02n":
+            return "cloud.sun"
+        case "03d", "03n":
+            return "smoke"
+        case "04d", "04n":
+            return "smoke.fill"
+        case "09d", "09n":
+            return "cloud.drizzle"
+        case "10d", "10n":
+            return "cloud.rain"
+        case "11d", "11n":
+            return "cloud.bolt"
+        case "13d", "13n":
+            return "snowflake"
+        case "50d", "50n":
+            return "humidity"
+        default: return "sun.min"
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -113,14 +140,21 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
         if collectionView == hourlyCollectionView {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HourlyCell", for: indexPath as IndexPath) as! HourlyCollectionViewCell
             cell.tempLabel.text = convertTempreture(temp: hourlyList?[indexPath.row].temp ?? 0, from: .kelvin, to: .celsius)
-            cell.timeLabel.text = date(timestamp: hourlyList?[indexPath.row].dt, format: "h a")
+            cell.timeLabel.text = date(timestamp: hourlyList?[indexPath.row].dt ?? 0, format: "h a")
             return cell
         }else{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DailyCell", for: indexPath as IndexPath) as! DailyCollectionViewCell
-            cell.dayLabel.text = date(timestamp: dailyList?[indexPath.row].dt, format: "EEEE")
+            cell.dayLabel.text = date(timestamp: dailyList?[indexPath.row].dt ?? 0, format: "EEEE")
             cell.descriptionLabel.text = dailyList?[indexPath.row].weather[0].weatherDescription.rawValue
             cell.minimumLabel.text = convertTempreture(temp: dailyList?[indexPath.row].temp.min ?? 0, from: .kelvin, to: .celsius)
             cell.maximumLabel.text = convertTempreture(temp: dailyList?[indexPath.row].temp.max ?? 0, from: .kelvin, to: .celsius)
+            
+            cell.setImage(icon: getIcon(icon: dailyList?[indexPath.row].weather[0].icon ?? ""))
+
+//            //call API icons
+//            cell.setUrlImage(from: URL(string: "https://openweathermap.org/img/w/\(dailyList?[indexPath.row].weather[0].icon ?? "03d").png")!)
+
+            
             return cell
         }
     }
@@ -152,10 +186,26 @@ class WeatherViewController: UIViewController , UICollectionViewDataSource, UICo
         feelsLikeLabel.isHidden = !flag
     }
     
-    func isDark(_ flag: Bool){
-        
+    @IBAction func modeButtonPressed(_ sender: UIButton) {
+        if isDark {
+            bgImageView.image = UIImage(named: "bg_night")
+            modeButton.setImage(UIImage(systemName: "moon"), for: .normal)
+            isDark = !isDark
+        }else{
+            bgImageView.image = UIImage(named: "bg_day")
+            modeButton.setImage(UIImage(systemName: "sun.max.fill"), for: .normal)
+            isDark = !isDark
+        }
     }
 
 }
+
+extension Array {
+   func slice(size: Int) -> [[Element]] {
+      (0...(count / size)).map{Array(self[($0 * size)..<(Swift.min($0 * size + size, count))])}
+   }
+}
+
+
 
 
--- WeatherApplication/View/DailyCollectionViewCell.swift
+++ WeatherApplication/View/DailyCollectionViewCell.swift
@@ -14,4 +14,37 @@ class DailyCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var minimumLabel: UILabel!
     @IBOutlet weak var maximumLabel: UILabel!
     
+    func setImage(icon: String){
+        imageView.image = UIImage(systemName: icon)
+    }
+    
+    func setUrlImage(from url: URL){
+        imageView.downloaded(from: url)
+    }
+    
+    func getImage(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+}
+//
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
 }
